f524f6ee945ab6e5e25b128c8b30036818ed4c70
theBigTank <codeplay.erniie@gmail.com>
2025-06-18 12:08:53 +0100

    updated theme.json

--- src/js/wp/block-styles.js
+++ src/js/wp/block-styles.js
@@ -2,6 +2,8 @@ export default function blockStyles() {
 	setTimeout(() => {
 		wp.blocks.unregisterBlockStyle('core/image', 'rounded');
 		wp.blocks.unregisterBlockStyle('core/heading', 'asterisk');
+		// wp.blocks.unregisterBlockStyle('core/button', 'outline');
+		// wp.blocks.unregisterBlockStyle('core/button', 'fill');
 	}, 1000);
 
 
